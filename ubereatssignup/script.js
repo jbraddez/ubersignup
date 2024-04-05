171eb67a5684a60a1d375492a45654784323d4af
jbraddez <157930657+jbraddez@users.noreply.github.com>
2024-04-05 15:12:34 +0100

    Add files via upload

---
+++ ubereatssignup/script.js
@@ -0,0 +1,8 @@
+getElementByClass('box').addEventListener("mouseover", function(){ 
+    this.style.color = "red"; 
+}); 
+
+getElementByClass('box').addEventListener("mouseout", function(){ 
+    this.style.color = "default"; 
+}); 
+
